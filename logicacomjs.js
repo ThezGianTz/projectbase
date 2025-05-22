e95fdee7ecb6dbb81d76bed0526cd4acbea89b89
ThezGianTz <samuel16.ti@hotmail.com>
2025-05-22 11:15:18 -0300

    Aqui eu vi let,console,e fiz uma calculadora

---
+++ logicacomjs.js
@@ -0,0 +1,43 @@
+/*let number1 = 80;
+let number2 = 40;
+let som = number1+number2
+console.log("A soma é: "+som)
+console.log("---------------------")
+let number3 = 80;
+let number4 = 40;
+let sub = number3-number4
+console.log("A subtração é: "+sub)
+console.log("---------------------")
+let number5 = 80;
+let number6 = 40;
+let mult = number5*number6
+console.log("A multiplicação é: "+mult)
+console.log("---------------------")
+let number7 = 80;
+let number8 = 40;
+let div = number7/number8
+console.log("A divisão é: "+div)
+console.log("---------------------")
+
+let number1 = 10;
+let number2 = 5;
+let som = number1+number2
+console.log("A soma é: "+som)
+console.log("---------------------")
+let sub = number1-number2
+console.log("A subtração é: "+sub)
+console.log("---------------------")
+let mult = number1*number2
+console.log("A multiplicação é: "+mult)
+console.log("---------------------")
+let div = number1/number2
+console.log("A divisão é: "+div)
+let nome = "Richard";
+let salario = 4500;
+let mestrab = 8;
+let decimo = salario/12*mestrab
+console.log(nome+" seu décimo é: R$"+decimo+", parabéns!!")*/
+
+const nome = "Paula"
+nome = "João"
+console.log(nome)
